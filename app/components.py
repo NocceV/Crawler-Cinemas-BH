ef80dacd213098acd3b5574bf870bd6eccdf77a0
Vitor Nocce <noccevitor@gmail.com>
2025-03-10 23:21:31 -0300

    Remove pycache and .sesskey

--- app/components.py
+++ app/components.py
@@ -5,22 +5,6 @@ def gerar_titulo(titulo, subtitulo):
         H1(titulo, style={"textAlign": "center"}),  # Centraliza o texto
         P(subtitulo, style={"textAlign": "center"}),  # Centraliza o texto
         Button(
-            "Pesquisar",
-            style={
-                "backgroundColor": "black",
-                "color": "white",
-                "border": "none",
-                "padding": "10px 20px",
-                "cursor": "pointer",
-                "marginTop": "10px",
-            }
-        ),
-        style={
-            "display": "flex",
-            "flexDirection": "column",
-            "justifyContent": "center",
-            "alignItems": "center",
-            "height": "100vh",
-            "backgroundColor": "white",
-        }
+            "Pesquisar"
+        )
     )
