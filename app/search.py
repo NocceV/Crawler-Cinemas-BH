bf2228315c21d54448150bab7d106cdb2068f875
Vitor Nocce <noccevitor@gmail.com>
2025-03-16 23:58:21 -0300

    Voltando o get nome

--- app/search.py
+++ app/search.py
@@ -31,7 +31,7 @@ class moviesResearcher:
         time.sleep(4)
 
     def get_siteName(self):
-        site_name = self.driver.find_element(By.CSS_SELECTOR, '.relative.my-5.scroll-mt-\[250px\].overflow-hidden.rounded-\[10px\].bg-ing-neutral-600.p-4') # .line-clamp-3.align-middle.text-lg.leading-none.lg\\:text-2xl
+        site_name = self.driver.find_element(By.CSS_SELECTOR, '.line-clamp-3.align-middle.text-lg.leading-none.lg\\:text-2xl') #class="relative my-5 scroll-mt-[250px] overflow-hidden rounded-[10px] bg-ing-neutral-600 p-4"
         return site_name.text
 
     def capture_movies(self):
