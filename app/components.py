41e465b9a058ed8d8067f899c2772d2c722e5f4f
Vitor Nocce <noccevitor@gmail.com>
2025-03-25 16:17:14 -0300

    Last ajusts

--- app/components.py
+++ app/components.py
@@ -1,13 +1,29 @@
-from fasthtml.common import Div, H1, P, A, Ul, Li
+from fasthtml.common import Div, H1, P, A, Ul, Li, Strong
 
 def gerar_titulo(titulo, subtitulo, movies_list):
+    # Agrupar filmes por cinema
+    cinema_dict = {}
+    for movie in movies_list:
+        cinema_name = movie["cinema"]
+        if cinema_name not in cinema_dict:
+            cinema_dict[cinema_name] = []
+        cinema_dict[cinema_name].append(movie)
+
     return Div(
         H1(titulo, style={"textAlign": "center"}),  
         P(subtitulo, style={"textAlign": "center"}), 
         Ul(
             *[
-                Li(f"🎬 {movie['title']} - ⏰ {'- '.join(movie['schedule'])} 📍 {movie['cinema']}")
-                for movie in movies_list
+                Li(
+                    Strong(f"📍 {cinema}"),  # Nome do cinema aparece uma vez
+                    Ul(
+                        *[
+                            Li(f"🎬 {movie['title']} - ⏰ {', '.join(movie['schedule'])},")
+                            for movie in movies
+                        ], style={"listStyleType": "none", "marginLeft": "20px"}
+                    )
+                )
+                for cinema, movies in cinema_dict.items()
             ], 
             style={"textAlign": "center", "listStyleType": "none"}
         ) if movies_list else P("Nenhum filme encontrado.", style={"textAlign": "center"}),
